0e9e6c82cb86fc9e328f309c221c7f6a47dee74a
sugiyama gaku <gaku@libera-inc.co.jp>
2025-07-15 12:29:28 +0900

    HTMLのlang属性を日本語に設定し、インデントを統一しました。また、PHPコードの出力を整形し、可読性を向上させました。

--- header.php
+++ header.php
@@ -1,74 +1,74 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width,initial-scale=1" />
-	<meta name="format-detection" content="telephone=no" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta name="format-detection" content="telephone=no" />
 
-	<!-- favicon/webclipicon -->
-	<link rel="shortcut icon" href="<?php echo esc_url( get_template_directory_uri() . '/favicon.ico' ); ?>" />
-	<link rel="apple-touch-icon" href="<?php echo esc_url( get_template_directory_uri() . '/webclip.png' ); ?>" />
+    <!-- favicon/webclipicon -->
+    <link rel="shortcut icon" href="<?php echo esc_url( get_template_directory_uri() . '/favicon.ico' ); ?>" />
+    <link rel="apple-touch-icon" href="<?php echo esc_url( get_template_directory_uri() . '/webclip.png' ); ?>" />
 
-	<!-- google fonts -->
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet" />
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet" />
 
-	<!-- css -->
-	<link href="<?php echo esc_url( get_template_directory_uri() . '/css/style.css' ); ?>" rel="stylesheet">
+    <!-- css -->
+    <link href="<?php echo esc_url( get_template_directory_uri() . '/css/style.css' ); ?>" rel="stylesheet">
 
-	<!-- js -->
-	<script src="<?php echo esc_url( get_template_directory_uri() . '/js/main.js' ); ?>" defer></script>
+    <!-- js -->
+    <script src="<?php echo esc_url( get_template_directory_uri() . '/js/main.js' ); ?>" defer></script>
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-	<!-- header -->
-	<header class="header">
-		<div class="header-head">
-			<div class="l-container">
-				<div class="header-head-inner">
-					<?php $tag = ( is_home() || is_front_page() ) ? 'h1' : 'p'; ?>
-					<<?php echo $tag; ?> class="header-logo">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-							<picture>
-								<source media="(max-width: 559px)" srcset="<?php echo esc_url( get_template_directory_uri() . '/img/logo-sp.png' ); ?> 1x,
+    <!-- header -->
+    <header class="header">
+        <div class="header-head">
+            <div class="l-container">
+                <div class="header-head-inner">
+                    <?php $tag = ( is_home() || is_front_page() ) ? 'h1' : 'p'; ?>
+                    <<?php echo $tag; ?> class="header-logo">
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                            <picture>
+                                <source media="(max-width: 559px)" srcset="<?php echo esc_url( get_template_directory_uri() . '/img/logo-sp.png' ); ?> 1x,
 				<?php echo esc_url( get_template_directory_uri() . '/img/logo-sp@2x.png' ); ?> 2x" />
-								<source media="(min-width: 560px)" srcset="<?php echo esc_url( get_template_directory_uri() . '/img/logo.png' ); ?> 1x,
+                                <source media="(min-width: 560px)" srcset="<?php echo esc_url( get_template_directory_uri() . '/img/logo.png' ); ?> 1x,
 				<?php echo esc_url( get_template_directory_uri() . '/img/logo@2x.png' ); ?> 2x" />
-								<img src="<?php echo esc_url( get_template_directory_uri() . '/img/logo@2x.png' ); ?>"
-									width="640" height="84" alt="武者への道 Presented by 模写修行" decoding="async"
-									loading="lazy" />
-							</picture>
+                                <img src="<?php echo esc_url( get_template_directory_uri() . '/img/logo@2x.png' ); ?>"
+                                    width="640" height="84" alt="武者への道 Presented by 模写修行" decoding="async"
+                                    loading="lazy" />
+                            </picture>
 
-						</a>
-					</<?php echo $tag; ?>>
+                        </a>
+                    </<?php echo $tag; ?>>
 
-					<div class="header-search">
-						<button class="header-search-button js-search-modal-open-button">記事検索</button>
-					</div>
+                    <div class="header-search">
+                        <button class="header-search-button js-search-modal-open-button">記事検索</button>
+                    </div>
 
-					<div class="c-sns">
-						<a href="https://www.google.com/" class="c-sns-icon" target="_blank" rel="noopener noreferrer">
-							<img src="<?php echo esc_url( get_template_directory_uri() . '/img/icon-sns-twitter.svg' ); ?>"
-								width="400" height="400" alt="twitter" decoding="async" loading="lazy" />
-						</a>
+                    <div class="c-sns">
+                        <a href="https://www.google.com/" class="c-sns-icon" target="_blank" rel="noopener noreferrer">
+                            <img src="<?php echo esc_url( get_template_directory_uri() . '/img/icon-sns-twitter.svg' ); ?>"
+                                width="400" height="400" alt="twitter" decoding="async" loading="lazy" />
+                        </a>
 
-						<a href="https://www.google.com/" class="c-sns-icon" target="_blank" rel="noopener noreferrer">
-							<img src="<?php echo esc_url( get_template_directory_uri() . '/img/icon-sns-facebook.svg' ); ?>"
-								width="1024" height="1024" alt="facebook" decoding="async" loading="lazy" />
-						</a>
+                        <a href="https://www.google.com/" class="c-sns-icon" target="_blank" rel="noopener noreferrer">
+                            <img src="<?php echo esc_url( get_template_directory_uri() . '/img/icon-sns-facebook.svg' ); ?>"
+                                width="1024" height="1024" alt="facebook" decoding="async" loading="lazy" />
+                        </a>
 
-					</div>
-				</div>
-			</div>
-		</div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-		<div class="header-nav">
-			<div class="l-container">
-				<?php
+        <div class="header-nav">
+            <div class="l-container">
+                <?php
 				wp_nav_menu(
 					array(
 						'theme_location' => 'header-menu',
@@ -76,9 +76,9 @@
 					)
 				);
 				?>
-			</div>
-		</div>
-	</header>
-	<!-- end header-->
+            </div>
+        </div>
+    </header>
+    <!-- end header-->
 
-	<?php get_template_part( 'template/search-form' ); ?>
+    <?php get_template_part( 'template/search-form' ); ?>
--- index.php
+++ index.php
@@ -1,116 +1,113 @@
 <?php get_header(); ?>
 
 <main>
-	<?php
+    <?php
 	if ( is_home() || is_front_page() ) :
 		?>
-	<!-- top-kv -->
-	<?php
+    <!-- top-kv -->
+    <?php
 		$pickup         = wp_get_nav_menu_items( 'pickup' );
 		$pickup_post_id = isset( $pickup[0]->object_id ) ? $pickup[0]->object_id : null;
 
 		if ( $pickup_post_id ) :
 			$item          = $pickup[0];
 			$pickup_post   = get_page( $pickup_post_id );
-			$post_category = get_the_category( $pickup_post_id );
 			$thumbnail_id  = get_post_thumbnail_id( $pickup_post_id );
 			?>
-	<div class="top-kv">
-		<div class="top-kv">
-			<div class="l-container">
-				<div class="top-kv-inner">
-					<article class="top-kv-recommend">
-						<a href="<?php echo esc_attr( $item->url ); ?>" class="top-kv-recommend-link">
+    <div class="top-kv">
+        <div class="l-container">
+            <div class="top-kv-inner">
+                <article class="top-kv-recommend">
+                    <a href="<?php echo esc_attr( $item->url ); ?>" class="top-kv-recommend-link">
 
-							<div class="top-kv-recommend-thumbnail">
-								<?php the_post_thumbnail( $thumbnail_id ); ?>
-							</div>
+                        <div class="top-kv-recommend-thumbnail">
+                            <?php echo wp_kses_post( get_the_post_thumbnail( $pickup_post_id ) ); ?>
+                        </div>
 
-							<div class="top-kv-recommend-body">
-								<?php display_category_label( $pickup_post_id ); ?>
+                        <div class="top-kv-recommend-body">
+                            <?php display_category_label( $pickup_post_id ); ?>
 
-								<h2 class="top-kv-recommend-title">
-									<?php echo esc_html( $pickup_post->post_title ); ?>
-								</h2>
+                            <h2 class="top-kv-recommend-title">
+                                <?php echo esc_html( $pickup_post->post_title ); ?>
+                            </h2>
 
-								<div class="top-kv-recommend-date">
-									<time datetime="<?php the_time( 'Y-m-d' ); ?>" class="c-date">
-										<?php echo mysql2date( 'Y-m-d', $pickup_post->post_date ); ?>
-									</time>
-								</div>
-							</div>
-						</a>
-					</article>
+                            <div class="top-kv-recommend-date">
+                                <time datetime="<?php the_time( 'Y-m-d' ); ?>" class="c-date">
+                                    <?php echo mysql2date( 'Y-m-d', $pickup_post->post_date ); ?>
+                                </time>
+                            </div>
+                        </div>
+                    </a>
+                </article>
 
-					<div class="top-kv-character">
-						<img srcset="<?php echo esc_url( get_template_directory_uri() . '/img/img-kv-character.png' ); ?> 1x, <?php echo esc_url( get_template_directory_uri() . '/img/img-kv-character@2x.png' ); ?> 2x, <?php echo esc_url( get_template_directory_uri() . '/img/img-kv-character@3x.png' ); ?> 3x"
-							src="<?php echo esc_url( get_template_directory_uri() . '/img/img-kv-character@2x.png' ); ?>"
-							width="400" height="569" alt="おすすめの記事" />
-					</div>
-				</div>
-			</div>
+                <div class="top-kv-character">
+                    <img srcset="<?php echo esc_url( get_template_directory_uri() . '/img/img-kv-character.png' ); ?> 1x, <?php echo esc_url( get_template_directory_uri() . '/img/img-kv-character@2x.png' ); ?> 2x, <?php echo esc_url( get_template_directory_uri() . '/img/img-kv-character@3x.png' ); ?> 3x"
+                        src="<?php echo esc_url( get_template_directory_uri() . '/img/img-kv-character@2x.png' ); ?>"
+                        width="400" height="569" alt="おすすめの記事" />
+                </div>
+            </div>
+        </div>
 
-			<div class="top-kv-treat">
-				<img srcset="<?php echo esc_url( get_template_directory_uri() . '/img/img-kv-treat.png' ); ?> 1x, <?php echo esc_url( get_template_directory_uri() . '/img/img-kv-treat@2x.png' ); ?> 2x"
-					src="<?php echo esc_url( get_template_directory_uri() . '/img/img-kv-treat@2x.png' ); ?>"
-					width="500" height="172" alt="" />
-			</div>
-		</div>
-	</div>
+        <div class="top-kv-treat">
+            <img srcset="<?php echo esc_url( get_template_directory_uri() . '/img/img-kv-treat.png' ); ?> 1x, <?php echo esc_url( get_template_directory_uri() . '/img/img-kv-treat@2x.png' ); ?> 2x"
+                src="<?php echo esc_url( get_template_directory_uri() . '/img/img-kv-treat@2x.png' ); ?>" width="500"
+                height="172" alt="" />
+        </div>
+    </div>
 
-	<?php endif; ?>
+    <?php endif; ?>
 
-	<!-- end top-kv -->
-	<?php else : ?>
-	<!-- page-kv -->
-	<div class="c-page-kv">
-		<div class="l-container">
-			<?php
+    <!-- end top-kv -->
+    <?php else : ?>
+    <!-- page-kv -->
+    <div class="c-page-kv">
+        <div class="l-container">
+            <?php
 			if ( is_search() ) :
 				?>
-			<h1 class="c-title-level1">
-				<?php if ( get_search_query() ) : ?>
-				『<?php echo get_search_query(); ?>』の
-				<?php endif; ?>検索結果
-			</h1>
-			<?php else : ?>
-			<h1 class="c-title-level1">『<?php the_archive_title(); ?>』の記事一覧</h1>
-			<?php endif; ?>
-		</div>
-	</div>
-	<!-- end page-kv -->
-	<?php endif; ?>
+            <h1 class="c-title-level1">
+                <?php if ( get_search_query() ) : ?>
+                『<?php echo esc_html( get_search_query() ); ?>』の
+                <?php endif; ?>検索結果
+            </h1>
+            <?php else : ?>
+            <h1 class="c-title-level1">『<?php the_archive_title(); ?>』の記事一覧</h1>
+            <?php endif; ?>
+        </div>
+    </div>
+    <!-- end page-kv -->
+    <?php endif; ?>
 
-	<div class="u-ptb">
-		<div class="l-container">
-			<?php if ( have_posts() ) : ?>
-			<!-- posts -->
-			<div class="c-posts c-posts--col3">
-				<?php
+    <div class="u-ptb">
+        <div class="l-container">
+            <?php if ( have_posts() ) : ?>
+            <!-- posts -->
+            <div class="c-posts c-posts--col3">
+                <?php
 				while ( have_posts() ) :
 					the_post();
 					?>
-				<article class="c-post">
-					<div class="c-meta">
-						<?php display_category_label(); ?>
-						<time datetime="<?php the_time( 'Y-m-d' ); ?>"
-							class="c-date"><?php the_time( get_option( 'date_format' ) ); ?></time>
-					</div>
+                <article class="c-post">
+                    <div class="c-meta">
+                        <?php display_category_label(); ?>
+                        <time datetime="<?php the_time( 'Y-m-d' ); ?>"
+                            class="c-date"><?php the_time( get_option( 'date_format' ) ); ?></time>
+                    </div>
 
-					<a href="<?php the_permalink(); ?>" class="c-post-thumbnail">
-						<?php the_post_thumbnail(); ?>
-					</a>
+                    <a href="<?php the_permalink(); ?>" class="c-post-thumbnail">
+                        <?php the_post_thumbnail(); ?>
+                    </a>
 
-					<h2 class="c-post-title">
-						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-					</h2>
-				</article>
-				<?php endwhile; ?>
-			</div>
-			<!-- end posts -->
+                    <h2 class="c-post-title">
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </h2>
+                </article>
+                <?php endwhile; ?>
+            </div>
+            <!-- end posts -->
 
-			<!-- pagination -->
-			<?php
+            <!-- pagination -->
+            <?php
 				the_posts_pagination(
 					array(
 						'mid_size'           => 1,
@@ -120,12 +117,12 @@
 					)
 				);
 			?>
-			<!-- end pagination -->
-		</div>
-	</div>
-	<?php else : ?>
-	<p>『<?php echo get_search_query(); ?>』の検索結果が見つかりませんでした。</p>
-	<?php endif; ?>
+            <!-- end pagination -->
+        </div>
+    </div>
+    <?php else : ?>
+    <p>『<?php echo get_search_query(); ?>』の検索結果が見つかりませんでした。</p>
+    <?php endif; ?>
 </main>
 
 <?php get_footer(); ?>
